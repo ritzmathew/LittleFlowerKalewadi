348f9765e41331ff9f74c4b78f3581983af2567f
mathewritz91@gmail.com <mathewritz91@gmail.com>
2021-02-04 10:50:40 +0000

    Add project files.

---
+++ LittleFlowerKalewadi/Server/Models/Enquiry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LittleFlowerKalewadi.Server.Models
+{
+
+    public class Enquiry
+    {
+        [Required]
+        public string ApplicantName { get; set; }
+        [Required]
+        public DateTime ApplicantDOB { get; set; }
+        [Required]
+        public string[] ParentNames { get; set; }
+        public string Source { get; set; }
+        [Required]
+        public string EMail { get; set; }
+        public Guid UniqueId { get; set; }
+    }
+}
---
+++ LittleFlowerKalewadi/Shared/Info.cs
@@ -0,0 +1,10 @@
+﻿namespace LittleFlowerKalewadi.Shared
+{
+    public class Info
+    {
+        public string Address => " Opp.Near Nathu Nadhe corner St. Alphansa HIgh School, Vijay Nagar, Kalewadi, Pimpri-Chinchwad, Maharashtra 411017, India";
+        public string EMail => " littleflowerkalewadi@gmail.com";
+        public string PhoneNumber => " +91 94208 62570";
+        public string OpenHours => " Temporarily closed - online classes only";
+    }
+}
