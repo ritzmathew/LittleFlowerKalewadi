5d8715c338a4d5c1a953830a8edfff4e991bd0fe
mathewritz91@gmail.com <mathewritz91@gmail.com>
2021-02-05 17:37:42 +0000

    Updated Address + login page styling

--- LittleFlowerKalewadi/Shared/Info.cs
+++ LittleFlowerKalewadi/Shared/Info.cs
@@ -2,7 +2,7 @@
 {
     public class Info
     {
-        public string Address => " Opp.Near Nathu Nadhe corner St. Alphansa HIgh School, Vijay Nagar, Kalewadi, Pimpri-Chinchwad, Maharashtra 411017, India";
+        public string Address => " Opp. St. Alphonsa High School, Vijay Nagar, Kalewadi, Pimpri-Chinchwad, Maharashtra 411017, India";
         public string EMail => " littleflowerkalewadi@gmail.com";
         public string PhoneNumber => " +91 94208 62570";
         public string OpenHours => " Temporarily closed - online classes only";
