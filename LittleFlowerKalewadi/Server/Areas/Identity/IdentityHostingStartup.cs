2b8de238c22eeb3b6ad8c96801f8096efddab9aa
mathewritz91@gmail.com <mathewritz91@gmail.com>
2021-02-05 16:23:56 +0000

    Added scafold for Identity

---
+++ LittleFlowerKalewadi/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LittleFlowerKalewadi.Server.Data;
+using LittleFlowerKalewadi.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LittleFlowerKalewadi.Server.Areas.Identity.IdentityHostingStartup))]
+namespace LittleFlowerKalewadi.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
